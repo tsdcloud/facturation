0c3589c32dae3776b29fb6857a11cbec40853951
GOBE <agobe@bfclimited.com>
2022-06-08 15:59:04 +0100

    ajout des attributs de la signature

---
+++ database/migrations/2022_06_08_145011_add_path_and_user_id_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('signatures', function (Blueprint $table) {
+            $table->string('path')->after('id');
+            $table->foreignId('user_id')->constrained()->after('path');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('signatures', function (Blueprint $table) {
+            //
+        });
+    }
+};
