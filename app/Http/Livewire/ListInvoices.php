3bcf54e5cc862e58009752f2e83f9e4de62a8f74
GOBE <agobe@bfclimited.com>
2022-06-10 12:14:36 +0100

    afficher uniquement les factures valides

--- app/Http/Livewire/ListInvoices.php
+++ app/Http/Livewire/ListInvoices.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\invoice;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class ListInvoices extends Component
@@ -13,7 +14,9 @@ class ListInvoices extends Component
     {
         return view('livewire.list-invoices',[
             'invoices' => invoice::where(function($query){
-                    $query->where('invoice_no','LIKE',"%{$this->search_invoice_no_tractor_trailer}%");
+                $query->where('invoice_no','LIKE',"%{$this->search_invoice_no_tractor_trailer}%");
+                $query->where('user_id', auth()->user()->id);
+                $query->where('status_invoice','validated');
             })->orderBy('created_at','DESC')->paginate(2),
         ]);
     }
