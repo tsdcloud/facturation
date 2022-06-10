c5d90b1efcfbc333b2b560773f97cab2dec9e06a
GOBE <agobe@bfclimited.com>
2022-06-10 15:07:23 +0100

    annuler une facture

--- app/Http/Livewire/ListInvoices.php
+++ app/Http/Livewire/ListInvoices.php
@@ -3,13 +3,15 @@
 namespace App\Http\Livewire;
 
 use App\Models\invoice;
-use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Illuminate\Support\Facades\Log;
 
 class ListInvoices extends Component
 {
     public $search_invoice_no_tractor_trailer;
-
+    public array $data = [];
+    protected $listeners = ['destroy'];
+    
     public function render()
     {
         return view('livewire.list-invoices',[
@@ -19,5 +21,23 @@ class ListInvoices extends Component
                 $query->where('status_invoice','validated');
             })->orderBy('created_at','DESC')->paginate(2),
         ]);
+    }
+
+    public function getInvoice($id){
+   
+        $ok = invoice::where('id',$id)->first();    
+        $this->data[] = $ok;
+    //    dd($this->data)        ;
+    }
+
+    public function cancelInvoice(){
+        try{
+            tap($this->data)->update(['status_invoice' => 'cancelling']);
+           $this->dispatchBrowserEvent('closeAlert');
+           session()->flash('message', 'facture enregistreé avec succès.');
+        }catch(\Exception $e){
+            Log::error(sprintf('%d'.$e->getMessage(), __METHOD__));
+            session()->flash('error', 'Une erreur c\'est produite lors de l\'annulation de la facture veuillez essayer à nouveau.');
+        }
     }
 }
--- resources/views/livewire/list-invoices.blade.php
+++ resources/views/livewire/list-invoices.blade.php
@@ -2,6 +2,18 @@
     <div class="tables-wrapper">
         <div class="row">
           <div class="col-lg-12">
+            @if(session()->has('message'))
+            <div id="alert-message" class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>{{session('message')}} </strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+        @if(session()->has('error'))
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>{{session('error')}} </strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
             <div class="card-style mb-30">
               <div
                 class="d-flex flex-wrap justify-content-between align-items-center py-3">
@@ -49,10 +61,27 @@
                         <p>{{$invoice->weighbridge->label}}</p>
                       </td>
                       <td>
-                        <div class="action">
-                            <a class="link-primary" target="_blank" href="{{route('show-pdf',$invoice->id)}}" >
-                                <i class="lni lni-printer"></i>
-                                </a>
+                        <div class="action justify-content-end">
+                          <button
+                            class="more-btn ml-10 dropdown-toggle"
+                            id="moreAction1"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                          >
+                            <i class="lni lni-more-alt"></i>
+                          </button>
+                          <ul
+                            class="dropdown-menu dropdown-menu-end"
+                            aria-labelledby="moreAction1"
+                          >
+                            <li class="dropdown-item">
+                             <a style="color:grey" class="link-primary" target="_blank" href="{{route('show-pdf',$invoice->id)}}">Imprimer la facture</a>
+                            </li>
+                            <li class="dropdown-item">
+                              <a href="javascript:void(0)" wire:click="getInvoice({{$invoice->id}})"
+                                data-bs-toggle="modal" data-bs-target="#ModalTree" class="text-gray">Annuler la facture</a>
+                            </li>
+                          </ul>
                         </div>
                       </td>
                     </tr>
@@ -80,4 +109,61 @@
         <!-- end row -->
       </div>    
 </div>
+
+{{-- modale d'annulation d'une facture --}}
+<div class="warning-modal">
+  <div  wire:ignore.self class="modal fade" data-bs-backdrop="static" data-bs-keyboard="false" id="ModalTree" tabindex="-1" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content card-style warning-card text-center">
+              <div class="modal-header px-0 border-0 d-flex justify-content-end ">
+                  <button class="border-0 bg-transparent h1" data-bs-dismiss="modal">
+                      <i class="lni lni-cross-circle"></i>
+                  </button>
+              </div>
+              <div class="modal-body">
+                  <div class="icon text-danger mb-20">
+                      <i class="lni lni-warning"></i>
+                  </div>
+                  <div class="content mb-30">
+                      <h2 class="mb-15">Attentions !</h2>
+                      <p class="text-sm text-medium">
+                          Vous êtes sur le point d'annuler cette facture
+                          ok
+                      </p>
+                     {{-- @isset($data)
+                       <p>ok</p>
+                       {{-- <p>Facture N°  {{$data->id}}</p> 
+                     @endisset --}}
+                      {{-- <p>Facture N°  {{$data}}</p> --}}
+
+                      {{-- @empty(!$data)
+                        <p>Facture N°  {{$data['invoice_no'] }}</p> 
+                        {{-- <p>Nom Client {{$data->customer->label}}</p>
+                        <p>N° Tracteur {{$data->myTractor->label}}</p>
+                        <p>N° Remorque {{$data->myTrailer->label}}</p>
+                        <p> Crée par {{$data->user->name}}</p> 
+                      @endempty --}}
+                  </div>
+                  <div class="action d-flex flex-wrap justify-content-center">
+                      <button data-bs-dismiss="modal" class="main-btn danger-btn btn-hover m-1"
+                      > Supprimer
+                      </button>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
 </div>
+</div>
+@push('scripts')
+    <script>
+        document.addEventListener('closeAlert', closeAlert);
+        function closeAlert(){
+            setTimeout(()=>{
+                let alertNode = document.querySelector('#alert-message');
+                let alert = new bootstrap.Alert(alertNode);
+                alert.close()
+            },3000)
+        }
+    </script>
+@endpush
