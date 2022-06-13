04c3d4b49e20cec4ddb2207519887d8e743cff28
Alex GOBE <alexgobe92@gmail.com>
2022-06-13 13:59:25 +0100

    mise en forme du texte de la page home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
             <div class="col-xl-3 col-lg-4 col-sm-6">
                 <div class="icon-card mb-30">
                     <div class="content">
-                        <h6 class="mb-10">Nb T de factures / jour</h6>
+                        <h6 class="mb-10">Nombre facture / jour</h6>
                             <h6 class="text-bold mb-10"> 0 </h6>
                     </div>
                 </div>
@@ -33,7 +33,7 @@
                 <div class="icon-card mb-30">
                     <div class="content">
                         <h6 class="mb-10">Factures annulées</h6>
-                        <h6 class="text-bold mb-10"> 0 FCFA</h6>
+                        <h6 class="text-bold mb-10"> 0 </h6>
                     </div>
                 </div>
                 <!-- End Icon Cart -->
