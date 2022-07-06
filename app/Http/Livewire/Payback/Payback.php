df877311c94e58f835b527874d1d8c13de774268
GOBE <agobe@bfclimited.com>
2022-07-06 19:45:30 +0100

    ajout de la recherche sur le tableau et afficher les éléments de facon decroissant

--- app/Http/Livewire/Payback/Payback.php
+++ app/Http/Livewire/Payback/Payback.php
@@ -7,11 +7,15 @@ use Livewire\Component;
 
 class Payback extends Component
 {
-    public $invoice, $id_invoice;
+    public $invoice, $id_invoice, $search_invoice_no_tractor_trailer;
     public function render()
     {
         return view('livewire.payback.payback',[
-            'refunds' => Invoice::where('isRefunded',false)->paginate(10),
+            'refunds' => Invoice::where('isRefunded',false)
+                       ->where(function($query){
+                        $query->where('invoice_no','LIKE',"%{$this->search_invoice_no_tractor_trailer}%");
+                       })  
+                       ->orderBy('created_at', 'DESC')->paginate(10),
         ]);
     }
 
