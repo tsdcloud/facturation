0a30e23a54b22327b2050453823377355f38ef50
leGeeKMaka <alexgobe92@gmail.com>
2022-09-09 09:42:40 +0100

    fix  and update : mise en surbrillance avec les couleur si entree ou sortie afficher d'autre attribut

--- resources/views/livewire/checkpoint/index.blade.php
+++ resources/views/livewire/checkpoint/index.blade.php
@@ -19,12 +19,43 @@
     <div class="row">
         <div class="col-sm-12 col-md-6 col-lg-12 mt-2">
             @foreach ($invoices as $invoice)
-                <div class="card-style mb-3">
-                    <p class="text-muted">Nom client : {{$invoice->customer->label }} </p>
-                    <p class="text-muted">Tracteur : {{$invoice->myTractor->label }}</p>
-                    <p class="text-muted">Remorque : {{optional($invoice->myTrailer)->label }}</p>
-                    <a href="{{route('checkpoint.detail',$invoice)}}"  class="stretched-link"></a>
-                </div>
+                @if ($invoice->seen_entry_control === null)
+                    <div class="card-style mb-3">
+                        <p class="text-muted">Nom client :
+                            {{ $invoice->customer->label }} </p>
+                        <p class="text-muted">Tracteur : {{ $invoice->myTractor->label }}</p>
+                        <p class="text-muted">Remorque : {{ optional($invoice->myTrailer)->label }}</p>
+                        <p class="text-muted">Chef de guerite : {{ $invoice->user->name }}</p>
+                        <p class="text-muted">Facturé le : {{ $invoice->created_at->format('d/m/Y H:i:s') }}</p>
+                        <a href="{{ route('checkpoint.detail', $invoice) }}" class="stretched-link"></a>
+                    </div>
+                @endif
+
+                {{-- affiche en noir si controle entrée ok --}}
+                @if ($invoice->seen_exit_control === null && $invoice->seen_entry_control != null)
+                    <div class="card-style mb-3" style="background-color: rgb(243, 186, 13)">
+                        <p class="text-muted" style="color: white !important">Nom client :
+                            {{ $invoice->customer->label }} </p>
+                        <p style="color: white !important">Tracteur : {{ $invoice->myTractor->label }}</p>
+                        <p style="color: white !important">Remorque : {{ optional($invoice->myTrailer)->label }}</p>
+                        <p style="color: white !important">Chef de guerite : {{ $invoice->user->name }}</p>
+                        <p style="color: white !important">Facturé le : {{ $invoice->created_at->format('d/m/Y H:i:s') }}</p>
+                        <a href="{{ route('checkpoint.detail', $invoice) }}" class="stretched-link"></a>
+                    </div>
+                @endif
+
+                {{-- entrée sortie ok --}}
+                @if ($invoice->seen_exit_control != null && $invoice->seen_entry_control != null)
+                    <div class="card-style mb-3" style="background-color: rgb(19, 97, 49)">
+                        <p class="text-muted" style="color: white !important">Nom client :
+                            {{ $invoice->customer->label }} </p>
+                        <p class="text-muted" style="color: white !important">Tracteur : {{ $invoice->myTractor->label }}</p>
+                        <p class="text-muted" style="color: white !important">Remorque : {{ optional($invoice->myTrailer)->label }}</p>
+                        <p class="text-muted" style="color: white !important">Chef de guerite : {{ $invoice->user->name }}</p>
+                        <p class="text-muted" style="color: white !important">Facturé le : {{ $invoice->created_at->format('d/m/Y H:i:s') }}</p>
+                        <a href="{{ route('checkpoint.detail', $invoice) }}" class="stretched-link"></a>
+                    </div>
+                @endif
             @endforeach
         </div>
     </div>
