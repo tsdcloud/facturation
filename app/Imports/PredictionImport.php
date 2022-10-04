65db1da90d88c5b43b9fdba8318c132bf108d26c
leGeeKMaka <alexgobe92@gmail.com>
2022-10-04 11:21:09 +0100

    explication du code en commentaire

--- app/Imports/PredictionImport.php
+++ app/Imports/PredictionImport.php
@@ -13,46 +13,45 @@ class PredictionImport implements ToModel, WithHeadingRow, WithChunkReading
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
+    /**
+     ** on fais un isset pour rattraper le mauvais template envoyé au client avec la mauvaise entête 
+     * ! ajouter le created_at 
+     ** row indique les colonnes du fichier excel
+    */
     public function model(array $row)
-    {
-         /**
-          * ! ajouter le created_at 
-         */
-        // row ce sont les lignes excel
+    {  
         // dd($row);
-        return new Prediction([
-           'partenaire'     => $row['partenaires'],
-           'tractor'     => $row['vehicules'],
-           'trailer'    => $row['remorques'], 
-           'container_number'    => $row['n_conteneur'], 
-           'seal_number'    => $row['nplomb'], 
-           'loader'    => $row['chargeur'], 
-           'product'    => $row['produit'], 
-           'user_id' => auth()->user()->id,
-           'operation' => $row['operations'],
-           'weighing_status' => 'En attente',
-           'created_at' => now(),
-           'updated_at' => now(),
-        ]);
-        // return new Prediction([
-        //     'partenaire'     => $row['partenaires'],
-        //     'tractor'     => $row['vehicules'],
-        //     'trailer'    => $row['remorques'], 
-        //     'container_number'    => $row['n_conteneur'], 
-        //     'seal_number'    => $row['nplomb'], 
-        //     'loader'    => $row['chargeur'], 
-        //     'product'    => $row['produit'], 
-        //     'user_id' => auth()->user()->id,
-        //     'operation' => $row['operations'],
-        //     'head_guerite_entry' => $row['noms_chefs_guerite'],
-        //     'guerite_entry' => $row['guerite_entree'],
-        //     'date_weighing_entry' => $row['date_pesee_entree'],
-        //     'weighing_in' => $row['pesee_entree'],
-        //     'head_geurite_output' => $row['nom_chef_de_geurite_sortie'],
-        //     'geurite_output' => $row['guerite_sortie'],
-        //     'date_weighing_output' => $row['date_pesee_sortie'],
-        //     'weighing_status' => $row['statut_de_la_pesee'],
-        //  ]);
+         if(isset($row['nplomb']))
+             return new Prediction([
+                'partenaire'     => $row['partenaires'],
+                'tractor'     => $row['vehicules'],
+                'trailer'    => $row['remorques'], 
+                'container_number'    => $row['n_conteneur'], 
+                'seal_number'    => $row['nplomb'], 
+                'loader'    => $row['chargeur'], 
+                'product'    => $row['produit'], 
+                'user_id' => auth()->user()->id,
+                'operation' => $row['operations'],
+                'weighing_status' => 'En attente',
+                'created_at' => now(),
+                'updated_at' => now(),
+                ]);
+
+        if(isset($row['n_plomb']))
+            return new Prediction([
+                'partenaire'     => $row['partenaires'],
+                'tractor'     => $row['vehicules'],
+                'trailer'    => $row['remorques'], 
+                'container_number'    => $row['n_conteneur'], 
+                'seal_number'    => $row['n_plomb'], 
+                'loader'    => $row['chargeur'], 
+                'product'    => $row['produit'], 
+                'user_id' => auth()->user()->id,
+                'operation' => $row['operations'],
+                'weighing_status' => 'En attente',
+                'created_at' => now(),
+                'updated_at' => now(),
+                ]);
     }
     public function chunkSize(): int
     {
